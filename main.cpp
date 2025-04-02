644698cec83c700b825222ce16cb64e9fe6119f5
DevAndreww <andrea.sette2@marconibari.edu.it>
2025-04-02 17:25:25 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,46 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int n, i, j;
+    float t;
+    string s;
+    bool scambio;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    cout << "Quanti studenti ci sono in una classe?" << endl;
+    cin >> n;
+    float voti[n];
+    string nomi[n];
+
+    i = 0;
+    while (i < n) {
+        cout << "Scrivi rispettivamente il nome e il voto dello studente." << endl;
+        cin >> nomi[i];
+        cin >> voti[i];
+        i = i + 1;
+    }
+    i = 0;
+    scambio = true;
+    while (i <= n - 1 && scambio) {
+        j = 0;
+        scambio = false;
+        while (j <= n - 2 - i) {
+            if (voti[j] > voti[j + 1]) {
+                t = voti[j];
+                voti[j] = voti[j + 1];
+                voti[j + 1] = t;
+                s = nomi[j];
+                nomi[j] = nomi[j + 1];
+                nomi[j + 1] = s;
+                scambio = true;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    i = 0;
+    while (i < n) {
+        cout << nomi[i] << " " << voti[i] << endl;
+        i = i + 1;
+    }
+    
+}
